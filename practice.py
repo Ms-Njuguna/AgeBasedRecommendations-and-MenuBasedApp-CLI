280263725b10c16ef989d09c1b663835ba3a5fa2
Patricia <patricianjuguna084@gmail.com>
2025-08-07 13:51:15 +0300

    Challenge 1 complete: elif and comparison operators

---
+++ practice.py
@@ -0,0 +1,28 @@
+# challenge 1
+
+age = int(input("How old are you? "))
+
+if age > 0 and age <= 12:
+    print("You're a kid! Go play outside")
+elif age >= 13 and age <= 17:
+    print("Teen vibes! Focus on school")
+elif age >= 18 and age <= 24:
+    print("Young adult! Explore & build")
+elif age >= 25 and age <= 40:
+    print("Prime years! Grind mode")
+elif age >= 41:
+    print("You're seasoned! Enjoy the wisdom")
+else:
+    print("Hmm... that doesn't look like a real age")
+
+
+# challenge 2
+
+
+
+
+# challenge 3
+
+
+
+
